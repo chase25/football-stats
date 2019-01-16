8a3665028c770e32d7726210035c83ae28cc259e
Andrew Collinson <ffloyd2000@gmail.com>
2019-01-16 22:48:25 +0000

    Included my own templates for masthead and footer.
    Carried out some base styling and addressed Sass variable includes.
    
    Additionally also working through a tutorial which explains the app-server files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppMasthead } from './masthead/masthead.component';
+import { AppFooter } from './app-footer/app-footer.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMasthead,
+    AppFooter,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/masthead/masthead.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-masthead',
+  templateUrl: './masthead.component.html',
+  styleUrls: ['./masthead.component.scss']
+})
+
+export class AppMasthead {
+
+}
