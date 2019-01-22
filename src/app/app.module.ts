f0c8a67879311b09ce0eda4ed56a45c49d39575d
Andrew Collinson <ffloyd2000@gmail.com>
2019-01-22 21:10:14 +0000

    Removed tutorial servers files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppMasthead } from './masthead/masthead.component';
 import { AppFooter } from './app-footer/app-footer.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { SkiplinksComponent } from './skiplinks/skiplinks.component';
 import { PrimaryNavComponent } from './primary-nav/primary-nav.component';
 import { SecondaryNavComponent } from './secondary-nav/secondary-nav.component';
@@ -19,8 +17,6 @@ import { DefaultLeagueTableComponent } from './default-league-table/default-leag
     AppComponent,
     AppMasthead,
     AppFooter,
-    ServerComponent,
-    ServersComponent,
     SkiplinksComponent,
     PrimaryNavComponent,
     SecondaryNavComponent,
